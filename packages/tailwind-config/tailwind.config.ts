f4acb6da01a7084ac3c780e9204c67eee3ab0e41
Nam Vo <nam.vh@carehealth.io>
2024-12-30 00:51:14 +0700

    feat: add Typography

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -1,7 +1,82 @@
 import type { Config } from 'tailwindcss';
 
 const config: Omit<Config, 'content'> = {
-  theme: {},
+  theme: {
+    fontSize: {
+      h1: [
+        '3rem',
+        {
+          lineHeight: '1.2em',
+          letterSpacing: '-0.02em',
+          fontWeight: 600,
+        },
+      ],
+      h2: [
+        '2.5rem',
+        {
+          lineHeight: '1.2em',
+          letterSpacing: '-0.02em',
+          fontWeight: 600,
+        },
+      ],
+      h3: [
+        '2rem',
+        {
+          lineHeight: '1.4em',
+          letterSpacing: '-0.02em',
+          fontWeight: 600,
+        },
+      ],
+      h4: [
+        '1.5rem',
+        {
+          lineHeight: '1.5em',
+          letterSpacing: '-0.02em',
+          fontWeight: 600,
+        },
+      ],
+      h5: [
+        '1.25rem',
+        {
+          lineHeight: '1.4em',
+          letterSpacing: '-0.02em',
+          fontWeight: 600,
+        },
+      ],
+      h6: [
+        '1.125rem',
+        {
+          lineHeight: '1.4em',
+          letterSpacing: '-0.02em',
+          fontWeight: 600,
+        },
+      ],
+      lg: [
+        '1.125rem',
+        {
+          lineHeight: '1.55em',
+        },
+      ],
+      md: [
+        '1rem',
+        {
+          lineHeight: '1.6em',
+        },
+      ],
+      sm: [
+        '0.875rem',
+        {
+          lineHeight: '1.55em',
+        },
+      ],
+      xs: [
+        '0.75rem',
+        {
+          lineHeight: '1.55em',
+        },
+      ],
+    },
+  },
 };
 
 export default config;
--- packages/ui/src/components/typography.tsx
+++ packages/ui/src/components/typography.tsx
@@ -1,12 +1,40 @@
 import cn from '@repo/utils/cn';
 import { PropsWithChildren } from 'react';
 
+export type TypographyVariant =
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'h4'
+  | 'h5'
+  | 'h6'
+  | 'lg'
+  | 'md'
+  | 'sm'
+  | 'xs';
+
 export interface TypographyProps extends PropsWithChildren {
   className?: string;
+  variant: TypographyVariant;
 }
 
-const Typography = ({ children, className }: TypographyProps) => {
-  return <span className={cn(className)}>{children}</span>;
+const variantStyles = {
+  h1: 'text-h1',
+  h2: 'text-h2',
+  h3: 'text-h3',
+  h4: 'text-h4',
+  h5: 'text-h5',
+  h6: 'text-h6',
+  lg: 'text-lg',
+  md: 'text-md',
+  sm: 'text-sm',
+  xs: 'text-xs',
+};
+
+const Typography = ({ children, className, variant }: TypographyProps) => {
+  return (
+    <span className={cn(variantStyles[variant], className)}>{children}</span>
+  );
 };
 
 export default Typography;
